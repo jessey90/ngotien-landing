4ce938da2ce807fc39c9f8c7011e97a3dd348ab1
Dinh Tuan Anh <dinhtuananh227@gmail.com>
2014-02-28 14:52:04 +0700

    28/2 rời VC

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="vi-vn" lang="vi-vn" dir="ltr">
+<head>
+    <title>Ngộ Tiên Landing</title>
+    <!--<base href="http://ngotien.sohagame.vn/" />-->
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Ngộ Tiên" />
+    <meta name="author" content="Ngộ Tiên" />
+    <meta name="robots" content="index, follow" />
+    <meta name="description" content="Ngộ Tiên." />
+    <meta name="generator" content="Ngộ Tiên" />
+    <link href="http://ngotien.sohagame.vn/" rel="canonical" />
+    <link href="/templates/sohagame/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+    <meta property="og:site_name" content="Ngộ Tiên" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Ngộ Tiên" />
+    <meta property="og:url" content="http://ngotien.sohagame.vn/" />
+    <meta property="og:description" content="Ngộ Tiên." />
+    <meta itemprop="name" content="Ngộ Tiên" />
+    <meta itemprop="description" content="Ngộ Tiên." />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+
+    <!--Insert CSS-->
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <script type="text/javascript">
+        //Google Analytics
+
+        $(document).ready(function(){
+
+            // hide #back-top first
+            $("#back-top").hide();
+
+            // fade in #back-top
+            $(function () {
+                $(window).scroll(function () {
+                    if ($(this).scrollTop() > 100) {
+                        $('#back-top').fadeIn();
+                    } else {
+                        $('#back-top').fadeOut();
+                    }
+                });
+
+                // scroll body to 0px on click
+                $('#back-top a').click(function () {
+                    $('body,html').animate({
+                        scrollTop: 0
+                    }, 800);
+                    return false;
+                });
+            });
+
+        });
+    </script>
+</head>
+<body>
+    <div class="wrapper1">
+        <div id="menu">
+            <div class="menu_top">
+                <a href="http://ngotien.sohagame.vn">Trang chủ</a>
+                <a href="http://forum.soha.vn/forums/1320-M2.html">Diễn đàn</a>
+                <a href="#">Sự kiện</a>
+            </div>
+        </div>
+        <div id="slogan">
+            <img src="img/slogan.png" alt="Slogan">
+        </div>
+        <div id="taigame">
+            <img src="img/taigame.png" alt="Tải game">
+        </div>
+    </div>
+
+    <div class="wrapper2">
+        <a name="boicanhgame"></a>
+        <div id="menu2">
+            <div class="menu_top_2">
+                <a href="#boicanhgame" id="boicanh">&nbsp</a>
+                <a href="#tranhdoanhchien" id="tranhdoanh">&nbsp</a>
+                <a href="#loidaichien" id="loidai">&nbsp</a>
+                <a href="#tinhang" id="tinhang">&nbsp</a>
+                <a href="#giaodien" id="giaodien">&nbsp</a>
+            </div>
+        </div>
+        <div id="boi_canh_game">
+            <div class="page"></div>
+            <div class="text">
+                <span>Thời Tam Hoàng Ngũ Đế, nhân giới xẩy ra đại chiến giữa Hoàng Đế và Xi Vưu. Ma giới vì muốn ảnh hưởng thế lực của mình đã phái Ma Sư- người mạnh nhất ma giới tham chiến và hỗ trợ Xi Vưu. Dưới sự giúp đỡ của Ma Sư, quân Xi Vưu 9 lần chiến đấu với Hoàng Đế thì chỉ có thắng hoặc hòa. Hoàng đế thấy vậy than trời, Nữ Oa bèn phái Chiến Thần và hàng loạt tiên nhân xuống trợ chiến</span>
+
+                <span>Trong một trận chiến, Ma Sư đánh bại Thần Giới và bắt được Lan Hoa Tiên, một trong những tiên tử đẹp nhất thần giới. Vẻ đẹp kiều diễm, cũng như khí khái của nàng đã khiến Ma Sư động lòng trắc ẩn bèn thả nàng đi. Cuộc chạm trám đó như là định mệnh, 2 người dần dần nảy sinh tình cảm. Ma Sư và Lan Hoa Tiên đã bí mật hẹn ước tranh xa tam giới sống với nhau </span>
+
+                <span>Mối nhân duyên ngang trái bị tiên giới phát hiện, Lan Hoa Tiên bị hành quyết, hồn phách bị chia nhỏ rơi vãi trong nhân giới tạo thành 12 mảnh linh thạch . Quá tức giận, Ma Sư đã tuyên bố rằng sẽ trả thù và tận diệt thần giới. Rốt cục ngày đen tối đối với tam giới cũng đến, Ma sư trở lại tam giới với sức mạnh hủy diệt gấp nhiều lần gieo giắc sự tàn bạo xuống tam giới một lần nữa…</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper3">
+        <a name="tranhdoanhchien"></a>
+        <div id="doanh_chien">
+            <div class="doanh_chien">
+                <span>
+                    Cuộc chiến tranh đoạt thiên hạ diễn ra vào tối thứ 6 hàng tuần, người chơi dành được ngôi vị quán quân sẽ tự động trở thành ĐẾ VƯƠNG và có đầy đủ QUYỀN HẠN của một ĐẾ VƯƠNG.
+                </span>
+
+                <span>
+                    1. Hoàng bào độc nhất vô nhị, giành được thuộc tính chiến đấu mạnh hơn <br>
+                    2. LĨnh bổng lộc hàng ngày <br>
+                    3. Bổ nhiệm và bãi miễn tiên hậu (ma hậu) <br>
+                    4. Giành 1 kỹ năng Đế vương: Quân lâm thiên hạ. <br>
+                    5. Phát triệu tập <br>
+                    6. Mỗi ngày được bỏ tù 3 lần người chơi cùng quốc gia <br>
+                    7. Có thể phóng thích người chơi Đế vương tự bỏ tù <br>
+                    8. Mỗi ngày có thể cấm ngôn người chơi trong quốc gia 10 lần, mỗi lần 60 phút. <br>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper4">
+        <a name="loidaichien"></a>
+        <div id="loi_dai">
+            <div class="loi_dai">
+
+            </div>
+        </div>
+    </div>
+    <p id="back-top">
+        <a href="#top"><span></span></a>
+    </p>
+</body>
+</html>
